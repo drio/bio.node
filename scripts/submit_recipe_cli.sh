8d05af74b06229a665795a3074243069bab55077
David Rio Deiros <driodeiros@gmail.com>
2011-05-20 15:19:48 -0500

    * Change statements in conditional

--- scripts/submit_recipe_cli.sh
+++ scripts/submit_recipe_cli.sh
@@ -7,15 +7,8 @@ b_path="/stornext/snfs0/rogers/drio_scratch/playground/test_pipe"
 bam1="$b_path/bam1"
 bam2="$b_path/bam2"
 
-if [ ! -t 0 ] # tty is a terminal
+if [ -t 0 ] # true(1) if opened and refers to a terminal
 then
-  cat - | curl -v \
-  -H "Content-Type: application/json" \
-  -X POST \
-  -d "@-" \
-  http://localhost:3000/execute_project
-  echo ""
-else
   cat <<EOF
 {
   "ref_fasta"   : "$ref",
@@ -25,4 +18,11 @@ else
   "sample_name" : "sample_test"
 }
 EOF
+else
+  cat - | curl -v \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "@-" \
+  http://localhost:3000/execute_project
+  echo ""
 fi
